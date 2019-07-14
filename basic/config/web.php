9b65d915e899cd82814f1cb4e586a64b06590866
P8e <alexey@sopdu.ru>
2019-07-15 00:37:51 +0300

    connect Russian language

--- basic/config/web.php
+++ basic/config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language'  => 'ru', // подключение русского языка
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
